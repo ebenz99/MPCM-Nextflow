0a2de782ec0ba4720a8eee4c818c25f7213c7c7e
Ethan Bensman <ebenz99@gmail.com>
2019-04-16 16:50:29 -0400

    adding python script to bin

---
+++ bin/test2.py
@@ -0,0 +1,47 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+from RunPrep import *
+from RunModel import *
+
+
+np.set_printoptions(threshold=np.nan)
+cdir = os.getcwd()
+ndir = cdir + "/op"
+os.makedirs(ndir,exist_ok=True)
+
+# deterministic
+th=12;
+flagD=1;
+[dataS, dataG] = RunPrep()
+STIM = np.zeros(shape = (775));
+STIM [84-1] = 0.00385
+[t, xoutG, xoutS] = RunModel(flagD, th, STIM, [], [], dataS, dataG, dataG.kTCleak, dataG.kTCmaxs)
+np.savetxt('t_deterministic.csv', t, delimiter=',')
+np.savetxt('xoutG_deterministic.csv', xoutG, delimiter=',')
+np.savetxt('xoutS_deterministic.csv', xoutS, delimiter=',')
+
+
+
+
+# stochastic
+th=12;
+flagD=0
+STIM = np.zeros(shape = (775));
+STIM[156-1:162]=[3.3,100,100,100,100,100,1721]
+[dataS, dataG] = RunPrep()
+[t, xoutG, xoutS] = RunModel(flagD, th, STIM, [], [], dataS, dataG, dataG.kTCleak, dataG.kTCmaxs)
+np.savetxt('t_stochastic.csv', t, delimiter=',')
+np.savetxt('xoutG_stochastic.csv', xoutG, delimiter=',')
+np.savetxt('xoutS_stochastic.csv', xoutS, delimiter=',')
+
+
+
+
+
+
+
+
+print("done ")
